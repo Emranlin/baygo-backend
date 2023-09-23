6b39087ef5f7cf75900c680927ccc885105e1580
ZHIIDEGUL <emranlini@gmail.com>
2023-09-24 00:24:38 +0600

    Refactor order

--- src/main/java/com/example/baygo/repository/OrderRepository.java
+++ src/main/java/com/example/baygo/repository/OrderRepository.java
@@ -35,12 +35,14 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
             "JOIN s.subProduct sp " +
             "JOIN sp.product p " +
             "JOIN o.buyer b " +
-            "WHERE p.seller.id = :sellerId " +
+            "JOIN p.subCategory sc " +
+            "JOIN sc.category c " +
+            "WHERE p.seller.id = :sellerId AND c.id = :categoryId " +
             "AND os.isFbbOrder = TRUE " +
             "AND (:keyword IS NULL OR p.name ILIKE %:keyword% OR b.fullName ILIKE %:keyword%) " +
             "AND (:status IS NULL OR os.orderStatus = :status) " +
             "ORDER BY o.dateOfOrder DESC")
-    Page<FBBOrderResponse> getAllOrders(Long sellerId, String keyword, OrderStatus status, Pageable pageable);
+    Page<FBBOrderResponse> getAllOrders(Long sellerId, String keyword, OrderStatus status, Long categoryId, Pageable pageable);
 
     @Query("SELECT NEW com.example.baygo.db.dto.response.fbs.FBSOrdersResponse(" +
             "o.id, os.id, sp.mainImage, s.barcode, os.fbsQuantity, p.name, sp.articulOfSeller, s.size, " +
